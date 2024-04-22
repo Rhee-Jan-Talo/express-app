be713bc268e849d185253a46f09296872e275e5c
Rhee-Jan-Talo <jarheetalo@gmail.com>
2024-04-22 22:52:45 +0800

    additional setup

--- app.js
+++ app.js
@@ -105,10 +105,9 @@ app.post("/upload", upload.single('image'), async (req, res) => {
 
 //If Else Kemeroot
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-//sample
 
 module.exports = app;
