dc4a6a2a5aa365ee475c1620f75ef3b070b4d4f9
Rhee-Jan-Talo <jarheetalo@gmail.com>
2024-04-22 23:34:27 +0800

    set new port to 3000

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.post("/upload", upload.single('image'), async (req, res) => {
 
 //If Else Kemeroot
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
